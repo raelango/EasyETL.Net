ff7cb5e31815adda4e2e0e565ee7465159239aed
raelango <33025783+raelango@users.noreply.github.com>
2017-10-23 11:09:29 -0400

    Add files via upload

--- MSMQListenerSample/Properties/Settings.Designer.cs
+++ MSMQListenerSample/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace RegexParserSolution.Properties {
+namespace MSMQListenerSample.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
